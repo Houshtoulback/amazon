67ba06727fea2c07f4553fe6c9a680c032d28ffb
Houshtoul's <hhaghpanah16@gmail.com>
2023-07-23 15:52:39 +0330

    just a beggining

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,21 @@
+import data from "./data";
+
 function App() {
     return (
         <div>
             <header className="bg-gray-500 p-4">
                 <a href="/">amazona</a>
             </header>
-            <main>list products</main>
+            <main>
+                <h1>Featured Products</h1>
+                {data.products.map((product) => (
+                    <div key={product.slug}>
+                        <img src={product.image} alt={product.name} />
+                        <p>{product.name}</p>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </main>
         </div>
     );
 }
